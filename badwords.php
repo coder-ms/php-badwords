f5d38577fc10893d60d2edd407f2a6cf29789505
coder-ms <marco.sbrascia@gmail.com>
2022-11-28 14:28:58 +0200

    finito

--- badwords.php
+++ badwords.php
@@ -1,12 +1,58 @@
+<?php
+
+$paragraph = $_POST['paragraph'];
+
+if (isset($_POST['banned'])) {
+    $badWord = $_POST['banned'];
+    $tabooWord = str_ireplace($badWord, '***', $paragraph);
+} else {
+    $bannedWord = '';
+    $tabooWord = 'Non è stata censurata nessuna parola!';
+}
+;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="./css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>PHP Censored Word</title>
 </head>
+
 <body>
-    
+    <div id="app">
+
+        <div class="wrapperx">
+
+            <div class="w-50 h-50 d-flex justify-content-center align-items-center">
+                <div class="leftBox text-center">
+                    <h4 class="text-center mb-2">FRASE DA CENSURARE</h4>
+                    <span class="text-center mb-2">Lunghezza paragrafo:
+                        <?php echo strlen($paragraph) . ' ' . 'caratteri'; ?>
+                    </span>
+                    <div class="text-center">
+                        <?php echo $paragraph; ?>
+                    </div>
+                </div>
+            </div>
+            <div class="w-50 h-50 d-flex justify-content-center align-items-center">
+                <div class="rightBox text-center">
+                    <h4 class="text-center mb-2">PAROLA CENSURATA</h4>
+                    <div class="text-center">
+                        <?php echo $tabooWord; ?>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="./css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>BADWORDS</title>
 </head>
+
 <body>
-    
+    <div class="wrappery d-flex justify-content-center align-items-center">
+        <form class="w-75 d-flex flex-column align-items-center" action="./badwords.php" method="POST">
+            <div class="mb-3 w-100">
+                <textarea class="form-control p-4" id="exampleFormControlTextarea1" name="paragraph"
+                    rows="3"></textarea>
+            </div>
+            <div class="input-group mb-3 w-50 d-flex flex-column align-items-center">
+                <span class="text-uppercase mt-4 mb-3" id="inputGroup-sizing-default">Parola da censurare</span>
+                <input type="text" minlength="4" class="bannedWord form-control w-100 rounded-pill text-center"
+                    name="banned">
+            </div>
+            <button type="submit" class="banBtn btn btn-outline-dark rounded-pill">
+                VERIFICA
+            </button>
+        </form>
+    </div>
 </body>
+
 </html>
